9eccbe7a39d487e5becd4df883e22fce738a3048
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-03-26 11:51:18 -0400

    created security group rules for bastion host and application load balancer

--- 10-SG/sg.tf
+++ 10-SG/sg.tf
@@ -47,3 +47,22 @@ module "app_alb_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id       = module.bastion_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
+
+# JDOPS-32, Bastion host should be accessed from office n/w
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
+}
