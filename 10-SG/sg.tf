3151a9e8ef353a90665517c0bc52b410d3e7408e
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-03-26 13:25:08 -0400

    app load balancer accepting traffic from VPN

--- 10-SG/sg.tf
+++ 10-SG/sg.tf
@@ -115,3 +115,13 @@ resource "aws_security_group_rule" "vpn_1194" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.vpn_sg.sg_id
 }
+
+#app alb accepting the traffic from the VPN
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
