9a3a66f3a79e0765a73a54836d437cb7c3ea7ca2
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-03-26 14:10:35 -0400

    sql server accepting traffic from bastion host

--- 10-SG/sg.tf
+++ 10-SG/sg.tf
@@ -125,3 +125,13 @@ resource "aws_security_group_rule" "app_alb_vpn" {
   source_security_group_id = module.vpn_sg.sg_id
   security_group_id = module.app_alb_sg.sg_id
 }
+
+#mysql accepting traffic from bastion 
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.bastion_sg.sg_id
+  security_group_id = module.mysql_sg.sg_id
+}
