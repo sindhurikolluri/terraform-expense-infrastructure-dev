d8ed556973e4209c253bf29d03c47fe148ff0e9c
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-03-26 12:33:20 -0400

    created security group and added ingress rules for our VPN

--- 10-SG/sg.tf
+++ 10-SG/sg.tf
@@ -48,6 +48,7 @@ module "app_alb_sg" {
     common_tags = var.common_tags
 }
 
+#app alb accepting traffic from bastion
 resource "aws_security_group_rule" "app_alb_bastion" {
   type              = "ingress"
   from_port         = 80
@@ -65,4 +66,52 @@ resource "aws_security_group_rule" "bastion_public" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.bastion_sg.sg_id
+}
+
+module "vpn_sg" {
+    source = "git::https://github.com/sindhurikolluri/terraform-aws-security-group-module.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "vpn"
+    sg_description = "Created for VPN instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #usually this should be static IP 
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
 }
